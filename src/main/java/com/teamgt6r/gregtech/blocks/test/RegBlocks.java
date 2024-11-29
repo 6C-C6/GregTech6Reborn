2ad49d712e6ef45e932106131e0e3153136f80ff
6C-C6 <113111472+6C-C6@users.noreply.github.com>
2024-11-29 11:38:41 +0800

    Bug fix
    make logo displays properly

--- src/main/java/com/teamgt6r/gregtech/blocks/test/RegBlocks.java
+++ src/main/java/com/teamgt6r/gregtech/blocks/test/RegBlocks.java
@@ -13,7 +13,7 @@ import net.neoforged.neoforge.registries.RegisterEvent;
 public class RegBlocks{
 	
   @SubscribeEvent
-  public void register(RegisterEvent  event){
+  public void register(RegisterEvent event){
     event.register(
     		BuiltInRegistries.BLOCK.key(),
     		registry -> {
@@ -21,7 +21,6 @@ public class RegBlocks{
     				registry.register(
     					ResourceLocation.fromNamespaceAndPath(GregTech6Reborn.MODID, "b"+Integer.toString(i)),
     					new Block(BlockBehaviour.Properties.of()
-    				      //.setId(ResourceKey.create(Registries.BLOCK,registryName))
     				      .destroyTime(2.0f)
     				      .explosionResistance(10.0f)
     				      .sound(SoundType.STONE)));
